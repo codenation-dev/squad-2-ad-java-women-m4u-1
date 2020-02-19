47f1cb41f8c4ac77b46b812b0bc4a0a7f85eecec
Bianca Mutaquiha <biafersil@gmail.com>
2020-02-18 23:21:02 -0300

    Projeto inicializado

---
+++ central-erros-api/src/main/java/br/com/codenation/centralerrosapi/CentralErrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenation.centralerrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralErrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralErrosApiApplication.class, args);
+	}
+
+}
---
+++ central-erros-api/src/test/java/br/com/codenation/centralerrosapi/CentralErrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codenation.centralerrosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralErrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
