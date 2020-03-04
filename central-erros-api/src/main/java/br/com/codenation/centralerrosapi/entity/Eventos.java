03c2db32f77cd948ba0dc459eb59b149489552f3
Bianca Mutaquiha <biafersil@gmail.com>
2020-03-03 23:18:10 -0300

    implementado entity Eventos e criado a tabela eventos

---
+++ central-erros-api/src/main/java/br/com/codenation/centralerrosapi/entity/Eventos.java
@@ -0,0 +1,114 @@
+package br.com.codenation.centralerrosapi.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Eventos {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Size(max = 50)
+    @NotNull
+    private String descricao;
+
+    @Size(max = 255)
+    @NotNull
+    private String log;
+
+    @NotNull
+    private char level;
+
+    @Size(max = 50)
+    private String origem;
+
+    @NotNull
+    private int quantidade;
+
+    @CreatedDate
+    private LocalDate dataEvento;
+
+    public Eventos(UUID id, String descricao, String log, char level, String origem, int quantidade, LocalDate dataEvento) {
+        this.id = id;
+        this.descricao = descricao;
+        this.log = log;
+        this.level = level;
+        this.origem = origem;
+        this.quantidade = quantidade;
+        this.dataEvento = dataEvento;
+    }
+
+    public Eventos() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    public char getLevel() {
+        return level;
+    }
+
+    public void setLevel(char level) {
+        this.level = level;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+}
